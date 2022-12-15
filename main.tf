ddb7b4266469bdf34b03b515921fe37f6343da53
svhanchenje <117143696+svhanchenje@users.noreply.github.com>
2022-12-15 14:40:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,8 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
+  ACCESS_KEY=var.ACCESS_KEY
+  SECRET_KEY=var.SECRET_KEY
 }
 resource "aws_instance" "example" {
   ami           =  "ami-074dc0a6f6c764218"
@@ -19,4 +21,13 @@ resource "aws_instance" "example" {
 
 tags = {
     Name = "terraform-example"
+  }
+
+  
+  variable "ACCESS_KEY" {
+  description = "The port the server will use for HTTP requests"
+  }
+  
+  variable "SECRET_KEY" {
+  description = "The port the server will use for HTTP requests"
   }
